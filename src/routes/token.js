b66936290906c31ee0d8bf06fa60567b3b8208e1
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-03-29 01:05:15 +0300

    token part completed.

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,9 +1,19 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
+const Token = require("../controllers/token");
 /* ------------------------------------------------------- */
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
+
+router.route("/").get(Token.list).post(Token.create);
+
+router
+  .route("/:id")
+  .get(Token.read)
+  .put(Token.update)
+  .patch(Token.update)
+  .delete(Token.delete);
